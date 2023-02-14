faa8c83eacb5a8cd32c786dc1c9939c0860ad2cc
Bogdan-SHk <bogdan.shkolnik@gmail.com>
2023-02-14 21:49:49 +0200

    Виконав завдання 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+// Створення і рендер розмітки
+
+const createGalleryMarkup = galleryItems => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>`;
+    })
+    .join("");
+};
+
+const galleryContainer = document.querySelector(".gallery");
+
+const cardMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardMarkup);
+
+
+// Ініціалізація бібліотеки SimpleLightbox
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: 'alt',
+  captionDelay: 250
+});
